86f3e3ef0a25882cc338302aefb217da9c9654cb
Sophia <sophiamdlen@gmail.com>
2026-09-05 12:28:30 +0700

    done login google

--- assignment/main.js
+++ assignment/main.js
@@ -5,6 +5,7 @@ import TWEEN from 'three/addons/libs/tween.module.js';
 import { TrackballControls } from 'three/addons/controls/TrackballControls.js'; 
 
 import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
+// import { color } from 'three/src/nodes/tsl/TSLCore.js';
 
 let camera;
 let scene; 
@@ -34,12 +35,260 @@ const people = [
         netWorth: 251260.8
     }, 
     {
-        name: "Prabowo Subianto", 
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
         photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
         age: 25, 
         country: "CN", 
         interest: "Writing", 
         netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
+    },
+    {
+        name: "Sophia Madlentsy Tambunan", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 25, 
+        country: "CN", 
+        interest: "Writing", 
+        netWorth: 251260.8
+    }, 
+    {
+        name: "New Yee Chian", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 23, 
+        country: "CN", 
+        interest: "Cooking", 
+        netWorth: 60393.60
+    }, 
+    {
+        name: "Wong Thiam Fook", 
+        photo: "https://static.kasatria.com/pivot-img/photo/019.jpg", 
+        age: 30, 
+        country: "CN", 
+        interest: "Travelling", 
+        netWorth: 146174.4
     }
 ]
 
@@ -47,6 +296,14 @@ function createTile(person, index){
     const square = document.createElement('div');
 
     square.className = "element";
+
+    if (person.netWorth < 100000){
+        square.style.backgroundColor = "red"
+    } else if (person.netWorth <= 200000){
+        square.style.backgroundColor = "orange"
+    } else {
+        square.style.backgroundColor = "green"
+    }
 
     square.innerHTML = `
         <img class="photo" src="${person.photo}" alt="${person.name}">
@@ -59,8 +316,11 @@ function createTile(person, index){
 
     const objectCss = new CSS3DObject(square);
 
-    objectCss.position.x = index * 250;
-    objectCss.position.y = 0;
+    const column = index % 20; 
+    const row = Math.floor(index/20);
+
+    objectCss.position.x = column * 150 - 1495;
+    objectCss.position.y = -row * 320;
     objectCss.position.z = 0;
 
     scene.add(objectCss);
@@ -90,7 +350,53 @@ function render(){
     renderer.render(scene, camera);
 }
 
+// Auth Goole 
+const CLIENT_ID = '594045206423-vf9jtit7ondi1o91j05r57jqcpadbi23.apps.googleusercontent.com';
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
+let client; 
+
+window.onGoogleLibraryLoad = () =>{
+    client = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID, 
+        scope: SCOPES, 
+        callback: onTokenResponse 
+    });
+
+    console.log('Google Oauth Ready');
+}
+
+const googleLoginButton = document.getElementById('google-login'); 
+
+googleLoginButton.addEventListener('click', ()=>{
+    client.requestAccessToken();
+})
+
+// function onTokenResponse(response) {
+//     console.log(response);
+// }
+
+// Spreadsheets
+const SPREADSHEETS_ID = '1wYeH6s9TFiMBJkJglY_b8H0WlQyFifhMBrr1PpZfm3o';
+const RANGE = 'Data Template.csv!A1:F201';
+
+function onTokenResponse(response){
+    console.log(response);
+
+    const accessToken = response.access_token;
+
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEETS_ID}/values/${encodeURIComponent(RANGE)}`
+
+    fetch(url,{
+        headers:{
+            Authorization: `Bearer ${accessToken}`
+        }
+    }).then((response) => response.json()).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.error(error);
+    });
+}
 
 animate();
 
